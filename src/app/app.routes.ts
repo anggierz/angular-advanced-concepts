b77abb3f96f1d575bdbe522be2bfb8583c7146d0
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-06 21:11:25 +0200

    program form component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: 'table',
         loadComponent: () => import('./components/table/table.component').then(m => m.TableComponent)
+    },
+    {
+        path: 'form',
+        loadComponent: () => import('./components/form/form.component').then(m => m.FormComponent)
     }
 ];
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
 
+  private readonly fb = inject(FormBuilder);
+  public form = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('^[a-zA-Z]+$')]],
+    email: ['', [Validators.required, Validators.email]],
+    city: ['', [Validators.required]],
+    country: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+    phone: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+    company: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+    age: ['', [Validators.required, Validators.min(0), Validators.max(100)]],
+  })
+
+  public onSubmit() {
+    if (this.form.valid) {
+      console.log('Form Submitted!', this.form.value);
+      alert('Usuario creado correctamente');
+      this.resetForm();
+    } else {
+      console.error('Form is invalid');
+      alert('Por favor, completa todos los campos del formulario correctamente.');
+    }
+  }
+  public resetForm() {
+    this.form.reset();
+  }
+  
 }
